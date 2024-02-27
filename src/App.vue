f4dc22c1e915162a5144747ce19a2933e7d04442
jjackchoi <jackchoi0325@gmail.com>
2024-02-27 21:46:39 +0900

    component, props 적용

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
     안녕하세요2
   </div>
 
-  <modal/>
+  <modal :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니"/>
 
   <div class="menu">
     <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
@@ -15,11 +15,13 @@
 
   <discount/>
 
-  <div v-for="(a,i) in 원룸들" :key="i">
+  <card :원룸="원룸들[i]" v-for="(a,i) in 원룸들" :key="a"/>
+
+  <!-- <div v-for="(a,i) in 원룸들" :key="i">
     <img :src="원룸들[i].image" class="room-img">
     <h4 @click="모달창열렸니 = true; 누른거 = i">{{ 원룸들[i].title }}</h4>
     <p>{{ 원룸들[i].price }}원</p>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -27,11 +29,13 @@
 import data from './assets/oneroom.js';
 import discount from './Discount.vue';
 import modal from './Modal.vue';
+import card from './Card.vue';
 
 export default {
   name: 'App',
   data(){ //데이터 보관함 state
     return {
+      오브젝트: { name: 'kim', age: 20 },
       누른거: 0,
       원룸들: data,
       모달창열렸니: false,
@@ -54,7 +58,8 @@ export default {
 
   components: {
     discount: discount,
-    modal: modal
+    modal: modal,
+    card: card
   }
 }
 </script>
--- src/Modal.vue
+++ src/Modal.vue
@@ -5,14 +5,22 @@
       <h4>{{ 원룸들[누른거].title }}</h4>  
       <p>{{ 원룸들[누른거].content }}</p>
       <p>{{ 원룸들[누른거].price }}원</p>
-      <button @click="모달창열렸니 = false">닫기</button>
+      <!-- <button @click="모달창열렸니 = false">닫기</button> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'ModalComponent'
+    name: 'ModalComponent',
+    data(){
+
+    },
+    props: {
+      원룸들: Array,
+      누른거: Number,
+      모달창열렸니: Boolean,
+    }
 }
 </script>
 
